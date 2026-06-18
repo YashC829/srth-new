9b205bc87d38adb197884e1a7c95a24eb43824a0
Yash Chadda <ychadda05@gmail.com>
2026-06-18 10:26:14 -0400

    added diffusion policy to init file

--- src/srth_new/low_level_policy/models/__init__.py
+++ src/srth_new/low_level_policy/models/__init__.py
@@ -1,5 +1,6 @@
 from .act_model import ACTPolicy
 from .dvrk_policy import DVRKPolicy
+from .diffusion_model import DiffusionPolicy
 
 #__all__ = ["build_act_model", "DVRKPolicy"]
-__all__ = ["ACTPolicy", "DVRKPolicy"]
+__all__ = ["ACTPolicy", "DVRKPolicy", "DiffusionPolicy"]
